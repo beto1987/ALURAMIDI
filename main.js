f2bc0d09ba45ca18038bcb8985a91073f0a6a860
Roberto <beto.garrido@gmail.com>
2024-04-08 15:58:45 -0300

    Alterações e melhorias no JS

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
+    
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -12,5 +20,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onclick = function () {
         tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function (event) {
+       console.log(event.code)
+       
+        if (event.code === 'Space' || event.code === 'Enter') {
+       tecla.classList.add('ativa');
+       }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 }
